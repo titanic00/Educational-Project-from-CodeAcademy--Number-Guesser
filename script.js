95e37302c48a45b2aedeb1735a24dedf21ded7f6
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-02-07 19:53:20 +0300

    added a function that determines which guess is closes to the target number

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let currentRoundNumber = 1;
 
 // My code is below:
 const generateTarget = () => Math.floor(Math.random() * 9);
+
+const compareGuesses = (humanGuess, computerGuess, targetToGuess) => {
+    if ((targetToGuess - humanGuess) < (computerGuess - targetGuess))
+        return true
+    else if ((targetToGuess - humanGuess) > (targetToGuess - computerGuess))
+        return false
+    else
+        return true
+}
